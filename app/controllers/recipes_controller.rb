47eda96fb6aabb6b8ac2ec54f25a00d7e8f30769
“ABSteingit config --global user.email “abs19stein@gmail.com <andrewstein@Andrews-MacBook-Pro-2.local>
2017-08-31 21:27:54 -0500

    additions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,28 @@
 class RecipesController < ApplicationController
-  def one_recipe_action
-    @recipe = Recipe.first
-    render 'one_recipe_page.html.erb'
+
+  def index 
+    @recipes = Recipe.all
+  end
+  
+  def new
+    
   end
 
-  def all_recipes_action
-    @recipes = Recipe.all
-    render 'all_recipes_page.html.erb'
+  def create
+    recipe = Recipe.new(
+                        title: params[:title],
+                        chef: params[:chef],
+                        ingredients: params[:ingredients],
+                        directions: params[:directions]
+
+                        )
+    recipe.save
   end
+
+  def show
+    @recipe = Recipe.find(params[:id]) 
+  end
+
 end
+
+
