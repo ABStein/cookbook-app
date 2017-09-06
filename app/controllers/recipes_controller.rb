0468aa9b8241516f4f05552cf373c0af63dcd4ed
“ABSteingit config --global user.email “abs19stein@gmail.com <andrewstein@Andrews-MacBook-Pro-2.local>
2017-09-05 21:28:09 -0500

    adding

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,9 @@
 class RecipesController < ApplicationController
 
-  def index 
+    def index
     @recipes = Recipe.all
   end
-  
+
   def new
     
   end
@@ -14,15 +14,42 @@ class RecipesController < ApplicationController
                         chef: params[:chef],
                         ingredients: params[:ingredients],
                         directions: params[:directions]
-
                         )
     recipe.save
+    flash[:success] = "Recipe successfully created" 
+    redirect_to "/recipes/#{recipe.id}"
   end
 
   def show
-    @recipe = Recipe.find(params[:id]) 
+    @recipe = Recipe.find(params[:id])
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    recipe = Recipe.find(params[:id])
+
+    recipe.assign_attributes(
+                            title: params[:title],
+                            chef: params[:chef],
+                            ingredients: params[:ingredients],
+                            directions: params[:directions]
+                            )
+
+    recipe.save
+    flash[:success] = "Recipe successfully updated"
+    redirect_to "/recipes/#{recipe.id}"
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    flash[:success] = "Recipe successfully deleted"
+    redirect_to "/recipes"
+  end
 end
+
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
   get '/' => 'recipes#index'
   get '/recipes' => 'recipes#index'
-  get '/recipes/new' 'recipes#new'
+  
+  get '/recipes/new' => 'recipes#new'
   post '/recipes' => 'recipes#create'
+  
   get '/recipes/:id' => 'recipes#show'
+  
+  get '/recipes/:id/edit' => 'recipes#edit'
+  patch'recipes/:id' => 'recipes#update'
 
+  delete '/recipes/:id' => 'recipes#destroy'
 end
